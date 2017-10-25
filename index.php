b6a64cf6e882620314f95d1d040744bef28bbe4c
Irmak <irmakali14@googlemail.com>
2017-10-25 13:08:11 +0200

    test commit

--- index.php
+++ index.php
@@ -10,9 +10,9 @@
     <title>my lovely home</title>
 </head>
 <body>
+<div class="container">
 <h1 class="mylovelyhome">my lovely home</h1>
 <hr/>
-
 <ul class="mainnavigation">
     <li><a href="index.php?page=account&action=logout">HOME DECOR</a></li>
     <li><a href="index.php?page=account&action=ansehen">HOME TEXTILES</a></li>
@@ -25,5 +25,22 @@
 <?php
 include "startseite/startseite.php";
 ?>
+
+
+<footer>
+    <hr/>
+    <ul class="mainnavigation">
+        <li><a href="index.php?page=account&action=logout">IMPRINT</a></li>
+        <li><a href="index.php?page=account&action=ansehen">CONTACT</a></li>
+        <li><a href="index.php?page=account&action=aendern">ABOUT US</a></li>
+        <li><a href="index.php?page=account&action=anzeigen">FAQ</a></li>
+        </li>
+    </ul>
+    <hr/>
+</footer>
+
+
+
+</div>
 </body>
 </html>
--- startseite/startseite.php
+++ startseite/startseite.php
@@ -1,5 +1,6 @@
 
-<img class="startseitenbild" src="images/JO-JUDY_Green_A3_Print_N_00_800x800@2x.jpg" alt="Startseite" width="1050" height="600">
+<img class="startseitenbild" src="images/JO-JUDY_Green_A3_Print_N_00_800x800@2x.jpg" alt="Startseite" >
+
 <div class="Oberueberschirft">NEW ARRIVALS</div>
 
 
