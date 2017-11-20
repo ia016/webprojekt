4d23f66b9f7ecc0e00766eb1d3211ed911ecf131
Irmak <irmakali14@googlemail.com>
2017-11-20 12:07:42 +0100

    :*

---
+++ inc/footer.php
@@ -0,0 +1,14 @@
+<footer>
+    <hr/>
+    <ul class="mainnavigation">
+        <li><a href="index.php?page=account&action=imprint">IMPRINT</a></li>
+        <li><a href="index.php?page=account&action=contact">CONTACT</a></li>
+        <li><a href="index.php?page=account&action=aboutus">ABOUT US</a></li>
+        <li><a href="index.php?page=account&action=faq">FAQ</a></li>
+        </li>
+    </ul>
+    <hr/>
+
+    <img class="footer" src="/hdm/webprojekt/images/instagram.png" width="25" height="25" alt="instagram">
+    <img class="footer" src="/hdm/webprojekt/images/facebook-letter-logo.png" width="25" height="25" alt="facebook">
+</footer>
