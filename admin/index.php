cc3f7a930576f17e7e1e831e30803d0a10280c6b
Irmak <irmakali14@googlemail.com>
2018-02-05 18:58:55 +0100

    Platzhalterbild geändert

--- admin/index.php
+++ admin/index.php
@@ -128,7 +128,7 @@ if (isset($_GET["addproduct"])) {
         $foto = $_FILES["foto"]["name"];
     } else if ($foto == "") {
         // Wenn kein Image gesetzt, dann Platzhalter
-        $foto = "platzhalter.PNG";
+        $foto = "noimage.PNG";
     }
 
     // Produkt speichern
